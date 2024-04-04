dee9b1c114d9289da0cb8e99acdcfc4fe6685b26
anjalisingh45 <163300398+anjalisingh45@users.noreply.github.com>
2024-04-04 08:29:31 -0400

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -51,7 +51,7 @@ export default function TextForm(props) {
   return (
     <>
       <div className="container" style={{color: props.mode==='dark'?'white' : 'black'}}>
-        <h1>{props.heading}</h1>
+        <h1 className="my-3">{props.heading}</h1>
         <div className="mb-3">
           <textarea
             className="form-control"
